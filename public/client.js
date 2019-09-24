87b52b674b7d5bf0eb8a05d1b1d28395841cbe9c
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-09-24 09:02:59 +0800

    fixing floats of divs and messages

--- public/client.js
+++ public/client.js
@@ -2,7 +2,6 @@ $(function () {
     var nickname = $('#nickname'); //user input nickname
     $('#logIn').click(function () {
         nickname = nickname.val();
-        console.log(nickname);
 
         $('header').show();
         $('.container').show();
@@ -11,6 +10,7 @@ $(function () {
         
         var socket = io();
         var users = []; //fixed list of users online 
+        var onlineCount = 0;
 
         /*code block for getting online users*/
         socket.emit('online', nickname);
@@ -19,12 +19,29 @@ $(function () {
             
             for(var i = 0; i < onlFr.length; i++){
                 if(onlFr[i]!=nickname && !users.includes(onlFr[i])){ //check if the online users is not in the array above 
-                    users.push(onlFr[i])
-                    $('#onlines').append($('<li>').text(onlFr[i]) );
+                    users.push(onlFr[i]);
+                    ++onlineCount;
+                    $('#onlines').append($('<div>').css({"width":"100%",'textAlign':'left',
+                    'marginBottom':'-1px','height':'24px','borderTop':'1px solid gray',
+                    'borderBottom':'1px solid gray'}).attr('id',onlineCount));
+                    $('#'+onlineCount).on('mouseover',function(){
+                        $(this).css({'backgroundColor':'#e6e6e6'});
+                    });
+                    $('#'+onlineCount).on('mouseout',function(){
+                        $(this).css({'backgroundColor':'white'});
+                    });
+
+                    $('#'+onlineCount).append($('<p>').css({'color':'green',
+                    'marginRight':'10px','marginLeft':'10px','fontWeight':'bold'}).text('•'));
+
+                    $('#'+onlineCount).append($('<p>').css({"marginTop":"-40px","marginLeft":"20px"}).text(onlFr[i]));
                 }
             }
             window.scrollTo(0, document.body.scrollHeight);
         })
+        $('#groupmessage').on('keypress', function(){
+            socket.emit('typing',{'sender':nickname,'message':$('#groupmessage').val()});
+        });
         /*end of getting online users*/
         
         $('#send').click(function(){
@@ -34,7 +51,6 @@ $(function () {
 
         //display typing status in the convo box
         socket.on('typing status', function(status){
-            console.log(status.message)
             if(status.message.length === 0){
                 $('#message').append($('<p>').addClass('status').text(status.sender+" is typing..."));
             }
@@ -43,17 +59,16 @@ $(function () {
         //typing status ends
 
         socket.on('chat message', function (msg) {
-            console.log(msg);
             var messageCount = msg.count;
-            console.log(messageCount);
             // var chatmessages = "";
-            if(msg.sender == nickname){
+            if(msg.sender == nickname && msg.message !== ""){
                 // chatmessages += msg.message;
+                console.log(msg.message.substring())
                 $('#message').append($('<div>').css({"width":"100%","float":"right"}).attr("id",messageCount.toString()))
                 $('#'+messageCount).append($('<div>').css({"textAlign":"right","padding":"5px","overFlow":"auto","float":"right",
                 "borderRadius":"15px","paddingRight":"20px","border":"1px solid black",
                 "width":"50%","background":"#9fff80"}).html(msg.message));
-            }else{
+            }else if(msg.message !== ""){
                 // chatmessages += (msg.sender+" : "+msg.message)
                 $('#message').append($('<div>').css({"width":"100%","float":"left"}).attr("id",messageCount.toString()))
                 $('#'+messageCount).append($('<p>').css({"textAlign":"left","padding":"5px","overFlow":"auto","float":"left",
